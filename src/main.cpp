e64ac9b20a5ff32ad31eb6efde14d9103d8e20fe
Cutler, Philip <greenthegarden@gmail.com>
2022-06-22 23:09:05 +0930

    added acs712 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -7,46 +7,67 @@
 #include <Wire.h>
 #include <Beastdevices_INA3221.h>
 #endif
+#if USE_ACS712
+#include <ACS712.h>
+#endif
 
 #define SERIAL_SPEED 115200 // serial baud rate
 #define PRINT_DEC_POINTS 3  // decimal points to print
 
 #if SD_IN_MKRZERO
-const unsigned int chipSelect = SDCARD_SS_PIN;
+        const unsigned int chipSelect = SDCARD_SS_PIN;
 #else
-const unsigned int chipSelect = 4;
+        const unsigned int chipSelect = 4;
 #endif
 
-#if USE_INA3221
-const unsigned long INA3221_PUBLISH_INTERVAL = 5000UL;
-unsigned long ina3221PreviousMillis = 0UL;
-#endif
-
-const unsigned int RELAY_1_PIN = 1;
-const unsigned int RELAY_2_PIN = 2;
-
-#if USE_INA3221
-// Set I2C address to 0x41 (A0 pin -> VCC)
-Beastdevices_INA3221 ina3221(INA3221_ADDR41_VCC);
-#endif
 
 #define BROKER_ADDR IPAddress(192, 168, 1, 186)
 // IPAddress brokerAddr;
 
 byte mac[] = {0x00, 0x10, 0xFA, 0x6E, 0x38, 0x4C};
-// unsigned long lastReadAt = millis();
-// unsigned long lastAvailabilityToggleAt = millis();
-// bool lastInputState = false;
-
 EthernetClient client;
+
 HADevice haDevice(mac, sizeof(mac));
 HAMqtt mqtt(client, haDevice);
 
 // Define relays as switches
 // Set initial state of off (false)
+void onBeforeSwitchStateChanged(bool state, HASwitch *s)
+{
+  // this callback will be called before publishing new state to HA
+  // in some cases there may be delay before onStateChanged is called due to network latency
+}
+
+// Configure Relay 1
+
+const unsigned int RELAY_1_PIN = 1;
+
 HASwitch relay_1("relay_1", false);
+
+void relay1onSwitchStateChanged(bool state, HASwitch *s)
+{
+  digitalWrite(RELAY_1_PIN, (state ? HIGH : LOW));
+}
+
+// Configure Relay 2
+
+const unsigned int RELAY_2_PIN = 2;
+
 HASwitch relay_2("relay_2", false);
+
+void relay2onSwitchStateChanged(bool state, HASwitch *s)
+{
+  digitalWrite(RELAY_2_PIN, (state ? HIGH : LOW));
+}
+
 #if USE_INA3221
+// Configure INA3221 Sensor
+const unsigned long INA3221_PUBLISH_INTERVAL = 5000UL;
+unsigned long ina3221PreviousMillis = 0UL;
+
+// Set I2C address to 0x41 (A0 pin -> VCC)
+Beastdevices_INA3221 ina3221(INA3221_ADDR41_VCC);
+
 HASensor ina3221_channel_1_current("ina3221_channel_1_current");
 HASensor ina3221_channel_1_voltage("ina3221_channel_1_voltage");
 HASensor ina3221_channel_2_current("ina3221_channel_2_current");
@@ -55,21 +76,19 @@ HASensor ina3221_channel_3_current("ina3221_channel_3_current");
 HASensor ina3221_channel_3_voltage("ina3221_channel_3_voltage");
 #endif
 
-void onBeforeSwitchStateChanged(bool state, HASwitch *s)
-{
-  // this callback will be called before publishing new state to HA
-  // in some cases there may be delay before onStateChanged is called due to network latency
-}
+#if USE_ACS712
+// Configure ACS712 Sensor
+const unsigned long ACS712_PUBLISH_INTERVAL = 5000UL;
+unsigned long acs712PreviousMillis = 0UL;
 
-void relay1onSwitchStateChanged(bool state, HASwitch *s)
-{
-  digitalWrite(RELAY_1_PIN, (state ? HIGH : LOW));
-}
+// Arduino UNO has 5.0 volt with a max ADC value of 1023 steps
+// ACS712 5A  uses 185 mV per A
+// ACS712 20A uses 100 mV per A
+// ACS712 30A uses  66 mV per A
+ACS712 ACS(A0, 5.0, 1023, 100);
 
-void relay2onSwitchStateChanged(bool state, HASwitch *s)
-{
-  digitalWrite(RELAY_2_PIN, (state ? HIGH : LOW));
-}
+HASensor acs712_current("acs712_current");
+#endif
 
 StaticJsonDocument<256> doc;
 
@@ -110,6 +129,10 @@ void setup()
 
   // Set shunt resistors to 10 mOhm for all channels
   ina3221.setShuntRes(10, 10, 10);
+#endif
+
+#if USE_ACS712
+  ACS.autoMidPoint();
 #endif
 
   Serial.println("\nInitializing SD card...");
@@ -197,6 +220,13 @@ void setup()
   ina3221_channel_3_voltage.setIcon("mdi:home-battery-outline");
 #endif
 
+#if USE_ACS712
+  acs712_current.setName("ACS712 Current");
+  acs712_current.setUnitOfMeasurement("A");
+  acs712_current.setDeviceClass("current");
+  acs712_current.setIcon("mdi:current-dc");
+#endif
+
   // you don't need to verify return status
   Ethernet.begin(mac);
 
@@ -210,19 +240,12 @@ void setup()
 
 void loop()
 {
-  String output;
-  serializeJson(doc, output);
-  Serial.println(output);
-  // Serial.println(relay_1.getName());
-  // Serial.println(relay_2.getName());
-  Serial.println(Ethernet.localIP());
-  delay(2000);
-
   Ethernet.maintain();
   mqtt.loop();
 
-#if USE_INA3221
   unsigned long now = millis();
+
+#if USE_INA3221
 
   // publish ina3221 values
   if (now - ina3221PreviousMillis >= INA3221_PUBLISH_INTERVAL)
@@ -234,6 +257,15 @@ void loop()
     ina3221_channel_2_voltage.setValue(ina3221.getVoltage(INA3221_CH2));
     ina3221_channel_3_current.setValue(ina3221.getCurrent(INA3221_CH3));
     ina3221_channel_3_voltage.setValue(ina3221.getVoltage(INA3221_CH3));
+  }
+#endif
+
+#if USE_ACS712
+  // publish acs712 values
+  if (now - acs712PreviousMillis >= ACS712_PUBLISH_INTERVAL)
+  {
+    acs712PreviousMillis = now;
+    acs712_current.setValue((double)ACS.mA_DC());
   }
 #endif
 }
